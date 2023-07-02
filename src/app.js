f7f8821c3790aeba4ed4a19a08cc4f3d09860f1a
Marcelo <macraze.info@gmail.com>
2023-07-02 18:59:01 -0500

    aaa

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ var routes_1 = __importDefault(require("./routes"));
 var app = (0, express_1["default"])();
 app.use((0, cors_1["default"])());
 app.use(express_1["default"].json());
-app.use('/restobar', routes_1["default"]);
+app.use('/api-restobar', routes_1["default"]);
 app.listen(20223, function () {
     return console.log('REST API server ready at: http://localhost:20223');
 });

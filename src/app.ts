35cf99353fc51e0138f9769552d9536a7076884f
Marcelo <macraze.info@gmail.com>
2024-01-11 21:15:31 -0500

    rai

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from "cors";
 
 import routes from "./routes";
+import { env } from 'process';
 
 const app = express()
 
@@ -10,6 +11,14 @@ app.use(express.json());
 
 app.use('/api-restobar', routes)
 
-app.listen(20223, () =>
+const portConect = env.PORT || 20223;
+
+app.get('/', function (req, res) {
+    res.status(200).json({ message: 'Estás conectado a nuestra API RESTOBAR port: '+ portConect })
+});
+
+
+
+app.listen(portConect, () =>
     console.log('REST API server ready at: http://localhost:20223'),
 )

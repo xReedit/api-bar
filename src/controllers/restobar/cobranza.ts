55a8acaefaf36d271375f35f176c0ec1cd597bc2
Marcelo <macraze.info@gmail.com>
2024-08-29 23:53:37 -0500

    cobranza

--- src/controllers/restobar/cobranza.ts
+++ src/controllers/restobar/cobranza.ts
@@ -20,8 +20,10 @@ router.get("/advertencia/:idsede", async (req, res) => {
         }
     });
     let rpt = rptData[0];
+
+    res.json({ mostrar: false });
     
-    if (rptData.length > 0) {
+    /*if (rptData.length > 0) {
         const ultimaFechaPago = rpt.ultimo_pago;
         const frecuenciaPago = rpt.frecuencia.toLowerCase();
         let fechaProximoPago: any;
@@ -128,7 +130,7 @@ router.get("/advertencia/:idsede", async (req, res) => {
         
     } else {
         res.json({ mostrar: false });
-    }
+    }*/
 });
 
 export default router;
